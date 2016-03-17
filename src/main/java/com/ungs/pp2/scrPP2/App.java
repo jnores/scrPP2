eb4eecc4ae94f57900aece40c81e9028ae47dd4a
yoshKnight <josenores89@gmail.com>
2016-03-17 00:15:26 -0300

    Primer commit

---
+++ src/main/java/com/ungs/pp2/scrPP2/App.java
@@ -0,0 +1,13 @@
+package com.ungs.pp2.scrPP2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
