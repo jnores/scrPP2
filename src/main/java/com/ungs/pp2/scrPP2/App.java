86c13bea36c18a6d56bc7b390220340e08e35887
yoshKnight <josenores89@gmail.com>
2016-03-18 21:00:17 -0300

    test de coverage.

--- src/main/java/com/ungs/pp2/scrPP2/App.java
+++ src/main/java/com/ungs/pp2/scrPP2/App.java
@@ -6,6 +6,10 @@ package com.ungs.pp2.scrPP2;
  */
 public class App 
 {
+	public int tres()
+	{
+		return 3;
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
