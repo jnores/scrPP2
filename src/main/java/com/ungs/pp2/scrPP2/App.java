52b74e8ab8851e341ce7a49b10084b9580973f2f
The_Rub <The_Rub@mail.com>
2016-03-20 19:26:48 -0300

    ejemplo mvc

--- src/main/java/com/ungs/pp2/scrPP2/App.java
+++ src/main/java/com/ungs/pp2/scrPP2/App.java
@@ -1,13 +1,36 @@
 package com.ungs.pp2.scrPP2;
 
-public class App 
-{
-	public int tres()
-	{
-		return 3;
+import com.ungs.pp2.scrPP2.controller.StudentController;
+import com.ungs.pp2.scrPP2.model.Student;
+import com.ungs.pp2.scrPP2.view.StudentView;
+
+public class App {
+	   public int tres()
+	   {
+			return 3;
+	   }
+	   public static void main(String[] args) {
+
+	      //fetch student record based on his roll no from the database
+	      Student model  = retriveStudentFromDatabase();
+
+	      //Create a view : to write student details on console
+	      StudentView view = new StudentView();
+
+	      StudentController controller = new StudentController(model, view);
+
+	      controller.updateView();
+
+	      //update model data
+	      controller.setStudentName("John");
+
+	      controller.updateView();
+	   }
+
+	   private static Student retriveStudentFromDatabase(){
+	      Student student = new Student();
+	      student.setName("Robert");
+	      student.setRollNo("10");
+	      return student;
+	   }
 	}
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/main/java/com/ungs/pp2/scrPP2/model/Student.java
@@ -0,0 +1,22 @@
+package com.ungs.pp2.scrPP2.model;
+
+public class Student {
+	   private String rollNo;
+	   private String name;
+	   
+	   public String getRollNo() {
+	      return rollNo;
+	   }
+	   
+	   public void setRollNo(String rollNo) {
+	      this.rollNo = rollNo;
+	   }
+	   
+	   public String getName() {
+	      return name;
+	   }
+	   
+	   public void setName(String name) {
+	      this.name = name;
+	   }
+	}
