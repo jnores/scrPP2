7fbdef1730a8c596bd9835b9162b09c939b9da85
Yosh Knight <josenores89@gmail.com>
2016-05-29 16:38:37 -0300

    se agrega test del command que muestra el formulario de alta de historia.

---
+++ src/test/java/pp2/scrum/dominio/comando/MostrarAgregarHistoriaTest.java
@@ -0,0 +1,34 @@
+package pp2.scrum.dominio.comando;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import junit.framework.TestCase;
+import pp2.scrum.dominio.comando.MostrarAgregarHistoria;
+
+public class MostrarAgregarHistoriaTest extends TestCase implements ActionListener{
+    MostrarAgregarHistoria command;
+    public MostrarAgregarHistoriaTest(String name) {
+        super(name);
+        command = new MostrarAgregarHistoria();
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public final void testExecuteHomeController() {
+        command.Execute(null);
+    }
+    
+    public final void testExecuteHomeControllerActionListener() {
+        command.Execute(null, this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
