ab66e837c5ac365b50a5c69238af4793bd918ffb
Yosh Knight <josenores89@gmail.com>
2016-05-29 17:34:01 -0300

    drop constructors not used from ViewUpdateEvent

--- src/main/java/pp2/scrum/view/events/ViewUpdateEvent.java
+++ src/main/java/pp2/scrum/view/events/ViewUpdateEvent.java
@@ -20,28 +20,4 @@ public class ViewUpdateEvent extends ActionEvent {
         super(source, id, command);
         // TODO Auto-generated constructor stub
     }
-
-    /**
-     * @param source
-     * @param id
-     * @param command
-     * @param modifiers
-     */
-    public ViewUpdateEvent(Object source, int id, String command, int modifiers) {
-        super(source, id, command, modifiers);
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @param source
-     * @param id
-     * @param command
-     * @param when
-     * @param modifiers
-     */
-    public ViewUpdateEvent(Object source, int id, String command, long when, int modifiers) {
-        super(source, id, command, when, modifiers);
-        // TODO Auto-generated constructor stub
-    }
-
 }
