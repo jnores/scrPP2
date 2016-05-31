73bedd2d759b315708b7ab53eadbeea412d16fc1
Yosh Knight <josenores89@gmail.com>
2016-05-30 21:41:06 -0300

    Se agrega una libreria pra testear gui.

--- src/test/java/pp2/scrum/dominio/comando/MostrarAgregarHistoriaTest.java
+++ src/test/java/pp2/scrum/dominio/comando/MostrarAgregarHistoriaTest.java
@@ -3,14 +3,18 @@ package pp2.scrum.dominio.comando;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JPanel;
+
 import junit.framework.TestCase;
-import pp2.scrum.dominio.comando.MostrarAgregarHistoria;
+import pp2.scrum.view.events.ViewUpdateEvent;
 
 public class MostrarAgregarHistoriaTest extends TestCase implements ActionListener{
     MostrarAgregarHistoria command;
+    JPanel panel;
     public MostrarAgregarHistoriaTest(String name) {
         super(name);
         command = new MostrarAgregarHistoria();
+        panel=null;
     }
 
     protected void setUp() throws Exception {
@@ -19,12 +23,14 @@ public class MostrarAgregarHistoriaTest extends TestCase implements ActionListen
     
     public final void testExecuteHomeControllerActionListener() {
         command.Execute(null, this);
+        assertNotNull(panel);     
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
+        if (e instanceof ViewUpdateEvent){
+            panel= (JPanel)e.getSource();
+        }        
     }
 
 }
