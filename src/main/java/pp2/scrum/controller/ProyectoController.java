58723759d9797110bc0213a45fbb023453701543
Yosh Knight <josenores89@gmail.com>
2016-06-03 02:20:48 -0300

    se cambia el tarea.avanzarEstado duplicado para finalizar una tarea por un while(tarea.estado < EstadoToDo)

--- src/main/java/pp2/scrum/controller/ProyectoController.java
+++ src/main/java/pp2/scrum/controller/ProyectoController.java
@@ -12,71 +12,71 @@ import pp2.scrum.dominio.interfaz.MailGateway;
 
 public class ProyectoController extends Controller 
 {
-	   private Proyecto proyecto;
+    private Proyecto proyecto;
 
-	   //La consulta se pasa a cada controller para hacer consultas a la base y son pasadas a su padre
-	   public ProyectoController(Proyecto model,MailGateway mailGateway) {
-		   super (mailGateway);
-	      this.proyecto = model;
-	   }
+    //La consulta se pasa a cada controller para hacer consultas a la base y son pasadas a su padre
+    public ProyectoController(Proyecto model,MailGateway mailGateway) {
+        super (mailGateway);
+        this.proyecto = model;
+    }
 
-	   /**
-	    * Genera una Lista de UserStories
-	    * @return List<UserStory> totalidad de userstories del proyecto.
-	    */
-	   public List<UserStoryHelper> getAllUserStories() {
-		   Set<UserStory> userStories = proyecto.getAllUserStories();
-		   ArrayList<UserStoryHelper> userStoriesHelpers = new ArrayList<UserStoryHelper>();
-		   for(UserStory userStory: userStories) {
-			   userStoriesHelpers.add( new UserStoryHelper(userStory,proyecto.getResponsable(userStory) ) );
-		   }
-			   
-		   return userStoriesHelpers;
-	   }
-	   
-	   /**
-	    * Genera una Lista de UserStories
-	    * @return List<UserStory> totalidad de userstories del proyecto.
-	    */
-	   public List<UserStoryHelper> getBacklog() {
-		   List<UserStory> userStories = proyecto.getBacklog();
-		   ArrayList<UserStoryHelper> userStoriesHelpers = new ArrayList<UserStoryHelper>();
-		   for(UserStory userStory: userStories) {
-			   userStoriesHelpers.add( new UserStoryHelper(userStory,proyecto.getResponsable(userStory) ) );
-		   }
-			   
-		   return userStoriesHelpers;
-	   }
-	   
-	   public UserStoryHelper getUserStoryHelper(int id) {
-		   UserStory userStory = proyecto.getUserStoryPorId(id);
-		   Miembro   miembro   = proyecto.getResponsable(userStory);
-		   return new UserStoryHelper(userStory,miembro);
-	   }
-	   
-	   public void setProyecto(Proyecto proyecto)
-	   {
-	      this.proyecto = proyecto;
-	   }
-	   
-	   public Proyecto getProyecto()
-      {
-         return proyecto;
-      }
-	   
-	   public MailGateway getMailGateway()
-      {
-         return mailGateway;
-      }
+    /**
+     * Genera una Lista de UserStories
+     * @return List<UserStory> totalidad de userstories del proyecto.
+     */
+    public List<UserStoryHelper> getAllUserStories() {
+        Set<UserStory> userStories = proyecto.getAllUserStories();
+        ArrayList<UserStoryHelper> userStoriesHelpers = new ArrayList<UserStoryHelper>();
+        for(UserStory userStory: userStories) {
+            userStoriesHelpers.add( new UserStoryHelper(userStory,proyecto.getResponsable(userStory) ) );
+        }
 
-	public long getSiguienteStoryID() {
-		return proyecto.getSiguienteStoryID();
-	}
+        return userStoriesHelpers;
+    }
 
-	public void agregarUserStory(UserStory us) {
-		if (us==null)
-			throw new InvalidParameterException("Se esperaba una user story para agregar y se recibio un elemento nulo.");
-		proyecto.addUserStory(us);
-		
-	}
+    /**
+     * Genera una Lista de UserStories
+     * @return List<UserStory> totalidad de userstories del proyecto.
+     */
+    public List<UserStoryHelper> getBacklog() {
+        List<UserStory> userStories = proyecto.getBacklog();
+        ArrayList<UserStoryHelper> userStoriesHelpers = new ArrayList<UserStoryHelper>();
+        for(UserStory userStory: userStories) {
+            userStoriesHelpers.add( new UserStoryHelper(userStory,proyecto.getResponsable(userStory) ) );
+        }
+
+        return userStoriesHelpers;
+    }
+
+    public UserStoryHelper getUserStoryHelper(int id) {
+        UserStory userStory = proyecto.getUserStoryPorId(id);
+        Miembro   miembro   = proyecto.getResponsable(userStory);
+        return new UserStoryHelper(userStory,miembro);
+    }
+
+    public void setProyecto(Proyecto proyecto)
+    {
+        this.proyecto = proyecto;
+    }
+
+    public Proyecto getProyecto()
+    {
+        return proyecto;
+    }
+
+    public MailGateway getMailGateway()
+    {
+        return mailGateway;
+    }
+
+    public long getSiguienteStoryID() {
+        return proyecto.getSiguienteStoryID();
+    }
+
+    public void agregarUserStory(UserStory us) {
+        if (us==null)
+            throw new InvalidParameterException("Se esperaba una user story para agregar y se recibio un elemento nulo.");
+        proyecto.addUserStory(us);
+
+    }
 }
--- src/main/java/pp2/scrum/controller/UserStoryPaginadoController.java
+++ src/main/java/pp2/scrum/controller/UserStoryPaginadoController.java
@@ -8,120 +8,121 @@ import pp2.scrum.dominio.Resultado;
 import pp2.scrum.dominio.entidad.Mail;
 import pp2.scrum.dominio.entidad.Tarea;
 import pp2.scrum.dominio.entidad.UserStory;
+import pp2.scrum.dominio.enums.Estado;
 import pp2.scrum.dominio.interfaz.MailGateway;
 
 public class UserStoryPaginadoController extends Controller 
 {
-	   private List<UserStory> model;
-	   private Paginacion paginaDefault;
-	   private Paginacion paginaActual;
-	   private int itemsTotales;
+    private List<UserStory> model;
+    private Paginacion paginaDefault;
+    private Paginacion paginaActual;
+    private int itemsTotales;
 
-	   //La consulta se pasa a cada controller para hacer consultas a la base y son pasadas a su padre
-	   public UserStoryPaginadoController(MailGateway mailGateway)
-	   {
-		  super (mailGateway);
-	      model = new ArrayList<UserStory>();
-	      paginaDefault = new Paginacion(null, null, 1, 5);
-	      paginaActual = paginaDefault;
-	      itemsTotales = model.size();
-	   }
+    //La consulta se pasa a cada controller para hacer consultas a la base y son pasadas a su padre
+    public UserStoryPaginadoController(MailGateway mailGateway)
+    {
+        super (mailGateway);
+        model = new ArrayList<UserStory>();
+        paginaDefault = new Paginacion(null, null, 1, 5);
+        paginaActual = paginaDefault;
+        itemsTotales = model.size();
+    }
 
-      public List<UserStory> getModel()
-      {
-         return model;
-      }
-      
-      public int getItemsTotales()
-      {
-         return itemsTotales;
-      }
-      
-      public int getPaginasTotales()
-      {
-         return itemsTotales / paginaActual.getItemsPorPagina() + (itemsTotales % paginaActual.getItemsPorPagina() == 0 ? 0 : 1);
-         //return itemsTotales != 0 ? (itemsTotales / paginaActual.getItemsPorPagina()) + 1 : 0 ;
-      }
-      
-      public Paginacion getPaginaActual()
-      {
-         return paginaActual;
-      }
+    public List<UserStory> getModel()
+    {
+        return model;
+    }
 
-      public void setModel(List<UserStory> model)
-      {
-         this.model = model;
-      }
-	   
-      public List<UserStory> ListarUserStories(Paginacion paginacion)
-      {
-         if (paginacion == null)
-         {
+    public int getItemsTotales()
+    {
+        return itemsTotales;
+    }
+
+    public int getPaginasTotales()
+    {
+        return itemsTotales / paginaActual.getItemsPorPagina() + (itemsTotales % paginaActual.getItemsPorPagina() == 0 ? 0 : 1);
+        //return itemsTotales != 0 ? (itemsTotales / paginaActual.getItemsPorPagina()) + 1 : 0 ;
+    }
+
+    public Paginacion getPaginaActual()
+    {
+        return paginaActual;
+    }
+
+    public void setModel(List<UserStory> model)
+    {
+        this.model = model;
+    }
+
+    public List<UserStory> ListarUserStories(Paginacion paginacion)
+    {
+        if (paginacion == null)
+        {
             paginacion = paginaDefault;
-         }                   
-         int itemsTotales = model.size();
-         int indice = (paginacion.getPagina() - 1) * paginacion.getItemsPorPagina();
-         List<UserStory> historias = new ArrayList<UserStory>();
-         int i = 0;
-         while(i < paginacion.getItemsPorPagina() && (indice + i) < itemsTotales && indice >= 0)
-         {
+        }                   
+        int itemsTotales = model.size();
+        int indice = (paginacion.getPagina() - 1) * paginacion.getItemsPorPagina();
+        List<UserStory> historias = new ArrayList<UserStory>();
+        int i = 0;
+        while(i < paginacion.getItemsPorPagina() && (indice + i) < itemsTotales && indice >= 0)
+        {
             UserStory story = model.get(indice + i);
             historias.add(new UserStory(story.getTitulo(), story.getDetalle(), story.getAutor(), story.getResponsable(), story.getHorasEstimadas(), story.getStoryPoints(), story.getIteracion(), story.getEstado(), null, null));       
             i++;
-         }
-         paginaActual = paginacion;
-         this.itemsTotales = itemsTotales;
-         return historias;
-      }
+        }
+        paginaActual = paginacion;
+        this.itemsTotales = itemsTotales;
+        return historias;
+    }
 
-      public List<UserStory> ObtenerPaginaAnterior()
-      { 
-         return paginaActual.getPagina() == 1 ? getModel() : ListarUserStories(new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), paginaActual.getPagina() - 1, paginaActual.getItemsPorPagina()));  
-      }
+    public List<UserStory> ObtenerPaginaAnterior()
+    { 
+        return paginaActual.getPagina() == 1 ? getModel() : ListarUserStories(new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), paginaActual.getPagina() - 1, paginaActual.getItemsPorPagina()));  
+    }
 
-      public List<UserStory> ObtenerPaginaSiguiente()
-      {
-         return paginaActual.getPagina() == getPaginasTotales() ? getModel() : ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), paginaActual.getPagina() + 1, paginaActual.getItemsPorPagina()));
-      }
+    public List<UserStory> ObtenerPaginaSiguiente()
+    {
+        return paginaActual.getPagina() == getPaginasTotales() ? getModel() : ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), paginaActual.getPagina() + 1, paginaActual.getItemsPorPagina()));
+    }
 
-      public List<UserStory> ObtenerPaginaPrimera()
-      {
-         return paginaActual.getPagina() == 1 ? getModel() : ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), 1, paginaActual.getItemsPorPagina()));
-      }
+    public List<UserStory> ObtenerPaginaPrimera()
+    {
+        return paginaActual.getPagina() == 1 ? getModel() : ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), 1, paginaActual.getItemsPorPagina()));
+    }
 
-      public List<UserStory> ObtenerPaginaUltima()
-      {
-         return paginaActual.getPagina() == getPaginasTotales() ? getModel() : ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), getPaginasTotales(), paginaActual.getItemsPorPagina()));
-      }
-      
-      public List<UserStory> ObtenerPaginaActual()
-      {
-         return ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), paginaActual.getPagina(), paginaActual.getItemsPorPagina()));
-      }
-      
-      public void finalizarStory(UserStory story)
-      {
-         List<Tarea> tareas = story.getTareas();
-         
-         for (Tarea tarea : tareas)
-         {
+    public List<UserStory> ObtenerPaginaUltima()
+    {
+        return paginaActual.getPagina() == getPaginasTotales() ? getModel() : ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), getPaginasTotales(), paginaActual.getItemsPorPagina()));
+    }
+
+    public List<UserStory> ObtenerPaginaActual()
+    {
+        return ListarUserStories( new Paginacion(paginaActual.getOrdenarPor(), paginaActual.getDireccionOrden(), paginaActual.getPagina(), paginaActual.getItemsPorPagina()));
+    }
+
+    public void finalizarStory(UserStory story)
+    {
+        List<Tarea> tareas = story.getTareas();
+
+        for (Tarea tarea : tareas)
+        {
             try
             {
-               tarea.avanzarEstado();
-               tarea.avanzarEstado();
+                while (tarea.getEstado().compareTo(Estado.ToDo) < 0)
+                    tarea.avanzarEstado();
             }
             catch(Exception ex){
-               
-            }            
-         }
-         
-      }
-      
-      public Resultado enviarHistoriaMail(UserStory story)
-      {
-         Mail mail = new Mail("julian.dirisio@gmail.com", "Tema", story.getTitulo());
-         return mailGateway.enviar(mail);
-      }
 
-	
+            }            
+        }
+
+    }
+
+    public Resultado enviarHistoriaMail(UserStory story)
+    {
+        Mail mail = new Mail("julian.dirisio@gmail.com", "Tema", story.getTitulo());
+        return mailGateway.enviar(mail);
+    }
+
+
 }
