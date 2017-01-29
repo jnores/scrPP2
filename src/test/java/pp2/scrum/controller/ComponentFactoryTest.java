07b023062ca9b20eedb24df9dc686da17a659876
Yosh Knight <josenores89@gmail.com>
2017-01-29 17:57:16 -0300

    Se agergan casos de prueba al factory de componenets

---
+++ src/test/java/pp2/scrum/controller/ComponentFactoryTest.java
@@ -0,0 +1,49 @@
+package pp2.scrum.controller;
+
+
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+/**
+ * @author yoshknight
+ *
+ */
+public class ComponentFactoryTest extends TestCase {
+
+    Properties properties;
+    public ComponentFactoryTest(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        properties = new Properties();
+        
+    }
+    
+ 
+    public void testContructionWithNullParameter() {
+        try {
+            new ComponentFactory(null);
+            assertTrue(false);
+        } catch (Exception e) {
+
+        }
+    }
+    
+    public void testContructionWithoutConfig() {
+        
+        ComponentFactory factory = new ComponentFactory(properties);
+        try {
+            factory.getComponentByName("dummyComponenet");
+            assertTrue(false);
+        } catch (NoSuchElementException | InstantiationException e) {
+            
+        }
+        
+    }
+    
+}
